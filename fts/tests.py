031502e8d4b2ed839f0aa72578d42f89ca54949e
Ju Liu <ju.liu@welaika.com>
2012-06-22 09:52:48 +0200

    first steps in python TDD

---
+++ fts/tests.py
@@ -0,0 +1,33 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PollsTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_poll_via_admin_site(self):
+        self.browser.get(self.live_server_url + '/admin/')
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('adm1n')
+        password_field.send_keys(Keys.RETURN)
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        polls_links = self.browser.find_elements_by_link_text('Polls')
+        self.assertEquals(len(polls_links), 2)
+
+        self.fail('todo: finish tests')
